2e4af1767edddb111599ceb3957f2f1be044cec7
kayodegigz <kayogunrinde@gmail.com>
2025-03-25 15:17:49 +0100

    append commands as list

--- packit/api.py
+++ packit/api.py
@@ -2346,7 +2346,7 @@ The first dist-git commit to be synced is '{short_hash}'.
             print(cmd)
 
         if osh_cli_args:
-            cmd + (shlex.split(osh_cli_args))
+            cmd.append(shlex.quote(osh_cli_args))
             print(cmd)
 
         cmd.append("--config=" + str(chroot))
