9ca4cefc124a69d3764cf018a3c6c0070bd81e13
kayodegigz <kayogunrinde@gmail.com>
2025-03-27 23:19:49 +0100

    debugging yaml file

--- packit/api.py
+++ packit/api.py
@@ -2355,6 +2355,7 @@ The first dist-git commit to be synced is '{short_hash}'.
         osh_options = self.package_config.srpm_build_deps
 
         print(osh_options)
+        print(f"Full package_config: {self.package_config.__dict__}")
 
         cmd.append("--config=" + str(chroot))
         cmd.append("--nowait")
